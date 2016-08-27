66cb338b41529e2cfad1f9981532591864b7d6b5
Eli Block <eli_block@live.com>
2016-08-27 03:16:48 -0700

    Support minified bundles via --production flag

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
-var path = require('path');
+let argv = require('yargs').argv;
+let path = require('path');
+let webpackUglifyJsPlugin = require('webpack-uglify-js-plugin');
+
+let plugins = [];
+
+if (argv.production) {
+    plugins.push(new webpackUglifyJsPlugin({
+        cacheFolder: path.resolve(__dirname, 'tmp/cached_uglify/'),
+        debug: true,
+        minimize: true,
+        sourceMap: false,
+        output: {
+            comments: false
+        }
+    }));
+}
 
 module.exports = {
     context: path.join(__dirname, 'lib'),
-
     entry: './index.js',
-
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js'
-    }
+    },
+    plugins: plugins   
 }
